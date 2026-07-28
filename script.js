bcc251337f9b6cf64b3a28156661bee911618b74
vicbaezarg <baezvic21@gmail.com>
2026-07-27 23:10:09 -0300

    Add random computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello world");
+
+function getComputerChoice () {
+    let words = ["rock", "paper", "scissors"]
+    let choice = Math.floor(Math.random() * words.length);
+    return words[choice];
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
